4076a3f8f4c61e8859d4dbe48a10c7c476a72208
mkosir <kosir.mar@gmail.com>
2022-08-24 11:49:03 +0200

    fix lint

--- components/TestComponent.tsx
+++ components/TestComponent.tsx
@@ -1,23 +1,23 @@
 export type TestComponentProps = {
   isEnabled: boolean;
-  shouldLint: () => void; // should be named onShouldLint
+  onShouldLint: () => void; // should be named onShouldLint
 };
 
 export const TestComponent = ({
   isEnabled,
-  shouldLint,
+  onShouldLint,
 }: TestComponentProps) => {
   console.log("isEnabled", isEnabled);
 
   // should be named handleComponentLog
-  const componentLog = () => {
+  const handleComponentLog = () => {
     console.log("componentLog");
   };
 
   return (
     <>
-      <div onClick={componentLog}>Test 1</div>
-      <div onClick={shouldLint}>Test 2</div>
+      <div onClick={handleComponentLog}>Test 1</div>
+      <div onClick={onShouldLint}>Test 2</div>
     </>
   );
 };
